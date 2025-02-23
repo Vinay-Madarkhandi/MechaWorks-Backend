99f8c80e0049a69dd011f9c2a967e445aabfb806
Madarkhandi Vinay Shantappa <139522065+Vinay-Madarkhandi@users.noreply.github.com>
2025-02-23 10:26:49 +0530

    Update privateKey.ts

--- src/privateKey.ts
+++ src/privateKey.ts
@@ -1,2 +1,2 @@
 
-export const privateKey = process.env.PRIVATE_KEY;
+export const privateKey = process.env.PRIVATE_KEY ?? "t7X9m5tzCqKuQHekh35eLhz6QubYNnucxs3yd23GnBtBfnQVvrPVafhFGtb3VKzsGCgc8JKXsr9iZbow5XJtzHu";
